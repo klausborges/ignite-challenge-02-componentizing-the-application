12652a0a1b10eff5f4adc34c225fcbdf868537d8
Klaus Borges <klausborgs@gmail.com>
2021-03-31 12:32:22 -0300

    feat: componentize Content and SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+import { GenreResponseProps as Genre } from "../App";
+
+interface SideBarProps {
+  genres: Genre[];
+  selectedGenreId: number;
+  onClickButton: (id: number) => void;
 }
+
+export function SideBar(props: SideBarProps) {
+  const { genres, selectedGenreId, onClickButton } = props;
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
+};
